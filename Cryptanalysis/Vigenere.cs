f64e24a5dd858abbd29987a13d77841232813dc1
Marine Thunet <marine.thunet@epita.fr>
2020-05-12 11:27:35 +0200

    moitiée du dernier

--- Cryptanalysis/Vigenere.cs
+++ Cryptanalysis/Vigenere.cs
@@ -5,19 +5,66 @@ namespace Cryptanalysis
 public class Vigenere
 {
     public const float FrenchIndexOfCoincidence = 0.0778F;
-    public Vigenere(string key)
+    private string key;
+    public Vigenere(string key)//ok
     {
-        throw new NotImplementedException();
+        this.key = key;
     }
 
-    public string Encrypt(string msg)
+    public string Encrypt(string msg)//ok
     {
-        throw new NotImplementedException();
+        string ret = "";
+        int imsg = 0;
+        int lmsg = msg.Length;
+        int ik = 0;
+        int lk = this.key.Length;
+        char test;
+        while (imsg<lmsg)
+        {
+            int c = Tools.LetterIndex(msg[imsg]);
+            if (c != -1)
+            { 
+                test=Tools.RotChar(msg[imsg], Tools.LetterIndex(key[ik]));
+                ret += test;
+                ik = Tools.Modulus(ik + 1, lk);
+            }
+            else
+            {
+                ret += msg[imsg];
+            }
+
+            imsg++;
+        }
+
+        return ret;
     }
 
-    public string Decrypt(string cypherText)
+    public string Decrypt(string cypherText)//ok
     {
-        throw new NotImplementedException();
+        string ret = "";
+        int imsg = 0;
+        int lmsg = cypherText.Length;
+        int ik = 0;
+        int lk = this.key.Length;
+        char test;
+        while (imsg<lmsg)
+        {
+            int c = Tools.LetterIndex(cypherText[imsg]);
+            if (c != -1)
+            { 
+                test=Tools.RotChar(cypherText[imsg], -Tools.LetterIndex(key[ik]));
+                ret += test;
+                ik = Tools.Modulus(ik + 1, lk);
+            }
+            else
+            {
+                ret += cypherText[imsg];
+            }
+
+            imsg++;
+        }
+
+        return ret;
     }
 
     public static string GuessKeyWithLength(string cypherText, int keyLength)
